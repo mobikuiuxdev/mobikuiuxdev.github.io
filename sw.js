c143f408ea156533cdbea2ececed6aded26cf90f
mobikuiuxdev <85950206+mobikuiuxdev@users.noreply.github.com>
2021-10-21 00:54:22 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,3 @@
-
-
 const cacheName="vre02",precacheResources=["/"];self.addEventListener("install",e=>{e.waitUntil(caches.open("vre02").then(e=>e.addAll(precacheResources)))}),addEventListener("activate",e=>{e.waitUntil(async
-
 function(){self.registration.navigationPreload&&await self.registration.navigationPreload.enable()}())}),addEventListener("fetch",e=>{e.respondWith(async
-
 function(){const t=await caches.match(e.request);if(t)return t;const a=await e.preloadResponse;return a||fetch(e.request)}())})
